f80d62d46273fe74bb897e27b3723ff62bd3f211
chanaka <hashan.chanaka96@gmail.com>
2025-02-13 05:25:32 +0000

    aws deplay part  update share lib

--- src/com/usrserver/Docker.groovy
+++ src/com/usrserver/Docker.groovy
@@ -53,10 +53,12 @@ class Docker {
 
 
     def Awsdeplay(String ImageName){
+        def dockercomds2 =  "export IMAGE_NAME=${ImageName}"
+        def dockercomds =  'docker-compose -f /home/ec2-user/docker-compose.yml up --detach'
         script.sshagent(['aws-ec2-userser']) {
             script.sh 'scp -o StrictHostKeyChecking=no docker-compose.yml ec2-user@13.60.196.175:/home/ec2-user'
-            script.sh "export IMAGE_NAME=${ImageName}"
-            script.sh 'docker-compose -f /home/ec2-user/docker-compose.yml up --detach'
+            script.sh "ssh -o StrictHostKeyChecking=no  ec2-user@13.60.196.175 ${dockercomds2}"
+            script.sh "ssh -o StrictHostKeyChecking=no  ec2-user@13.60.196.175 ${dockercomds}"
         }
     }
 }
